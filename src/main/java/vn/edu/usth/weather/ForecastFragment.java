0c91bde6e54ddfb356409311224e74473bdf552e
V8CrossPlane <linhtd.b8099@st.usth.edu.vn>
2019-11-20 12:40:36 +0700

    Lab_8

---
+++ src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -0,0 +1,69 @@
+package vn.edu.usth.weather;
+
+import android.os.Bundle;
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.widget.ImageView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.ScrollView;
+import java.util.Random;
+
+
+public class ForecastFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,@Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        ScrollView scrollView = (ScrollView) inflater.inflate(R.layout.fragment_forecast, container, false);
+        LinearLayout linearLayout = scrollView.findViewById(R.id.weather);
+        LinearLayout row;
+        TextView txt;
+        ImageView icon;
+        TextView weather_txt;
+
+
+        String days[] = {"Mon", "Tue", "Wed", "Thurs", "Fri", "Sat", "Sun"};
+        String weather[] = {"Rain", "Sunny", "Heavy Rain", "Cloudy"};
+
+        Random r = new Random();
+        int a;
+
+        for(int i = 0; i < 20; i ++) {
+            row = (LinearLayout) inflater.inflate(R.layout.row, container, false);
+
+            txt = row.findViewById(R.id.txt_day);
+            icon = row.findViewById(R.id.icon);
+            weather_txt = row.findViewById(R.id.weather_txt);
+
+            a = r.nextInt(50);
+            txt.setText(days[i % 7]);
+            weather_txt.setText(weather[a % 4]);
+            switch (a % 4) {
+                case 0:
+                    icon.setImageResource(R.drawable.rain);
+                    break;
+                case 1:
+                    icon.setImageResource(R.drawable.sunny);
+                    break;
+                case 2:
+                    icon.setImageResource(R.drawable.thunderstorm);
+                    break;
+                case 3:
+                    icon.setImageResource(R.drawable.cloudy);
+                    break;
+            }
+            linearLayout.addView(row);
+        }
+        return scrollView;
+    }
+
+}
---
+++ src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,51 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import android.os.Bundle;
+import android.util.Log;
+import com.google.android.material.tabs.TabLayout;
+
+public class WeatherActivity extends AppCompatActivity {
+    private static final String TAG = "Weather Activity";
+    private ViewPager viewPager;
+    private Adapter adapter;
+    private TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        Log.i("Create", "Call Create");
+        viewPager = findViewById(R.id.pager);
+        adapter = new Adapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Abc", "Def");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("bac", "edf");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("acb", "Dfe");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("BCA", "EDF");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Cab", "fDe");
+    }
+}
